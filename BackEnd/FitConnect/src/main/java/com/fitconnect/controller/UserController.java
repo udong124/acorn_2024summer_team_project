9ae6c6223a20ebc3ea17193c24632c970695b150
udong <udong124@gmail.com>
2024-09-30 16:18:22 +0900

    checkUserName 추가

--- BackEnd/FitConnect/src/main/java/com/fitconnect/controller/UserController.java
+++ BackEnd/FitConnect/src/main/java/com/fitconnect/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -164,5 +165,12 @@ public class UserController {
 			isSuccess = userDao.updateRole(dto);
 		}
 		return Map.of("isSuccess", isSuccess);
+	}
+
+	//경로 변수에 전달되는 입력한 userName 이 사용가능한지 여부를 json 으로 응답하는 메소드 
+	@GetMapping("/user/check/{userName}")
+	public Map<String, Object> checkUserName(@PathVariable("userName") String userName){
+		
+		return Map.of("canUse", userService.canUse(userName));
 	}
 }
