05cbc907c35e139dc6a92cfcc3edf8897eb353a3
Crush on Study <github yeoneyam1219@gmail.com>
2024-10-07 07:28:20 +0900

    calendar, dietadd, dietjournal

--- FrontEnd/fitconnect/src/pages/Member/MemberExerciseAdd.js
+++ FrontEnd/fitconnect/src/pages/Member/MemberExerciseAdd.js
@@ -1,130 +1,119 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { Card, Button, Row, Col, Dropdown, DropdownButton, InputGroup, Form, Table, Modal, Alert } from "react-bootstrap";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"
 
-/**
- * MemberExerciseAdd 컴포넌트
- * - 사용자가 운동을 추가하거나 수정할 수 있는 기능 제공
- * - 운동 목록을 검색 및 선택하여 운동 리스트에 추가
- * - 추가된 운동을 드래그 앤 드롭으로 순서 조정 가능
- * - 운동의 무게, 횟수, 세트를 입력하고 저장할 수 있는 기능 제공
- */
-
 function MemberExerciseAdd() {
-  const [exercise_category, setExercise_Category] = useState(null)//API에서 받은 카테고리
-  const [e_journal_id, setEJournalId] = useState(null)//API에서 받은 exercise_id
-  const [m_calendar_id, setMCalendarId] = useState(null)//API에서 받은 m_Calendar_id
+  const { exercise_category, exercise_id } = useParams();
 
-  const navigate = useNavigate()//페이지 이동 및 쿼리파라미터 연관
-  const location = useLocation()//쿼리파라미터의 위치정보를 담기위함
-  const queryParams = new URLSearchParams(location.search)//URL 쿼리 파라미터
+  const navigate = useNavigate()
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
     
-  //날짜 관련 및 아무런 날짜 받은게 없으면 현재날짜 기준으로 셋팅
   const today = new Date()
   const localDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`
   const initialDateStr = queryParams.get("date") ? queryParams.get("date") : localDate
   const initialDate = new Date(initialDateStr)
   const [selectedDate, setSelectedDate] = useState(initialDate);
 
-  const [exerciseCategory, setExerciseCategory] = useState("all"); // 운동목록 선택
+  const [exerciseCategory, setExerciseCategory] = useState("all");
   const [exerciseData, setExerciseData] = useState([]);
-  const [search, setSearch] = useState(""); // 검색
+  const [search, setSearch] = useState("");
   const [selectExercise, setSelectExercise] = useState([]);
 
-  // 모달관련
   const [showModal, setShowModal] = useState(false);
   const [modalData, setModalData] = useState(null);
 
-  // 체크박스 옵션관련
   const [showDetailsOption, setShowDetailsOption] = useState(false);
 
   const token = localStorage.getItem('token')
 
-  //API에서 m_calnedar_id 받기
-  useEffect(()=>{
-    axios.get('/membercalendar')
-    .then(res=>{setMCalendarId(res.data.m_calendar_id)})
-    .catch(error=> console.log(error))
-  })
+  const [m_calendar_id, setMCalendarId] = useState(null);
+  const [m_calendar_id_max, setMCalendarIdMax] = useState(null);
 
-  //해당날짜 쿼리파라미터로 로딩시 가져오기
-  useEffect(()=>{
-    if(!queryParams.get("date")){
-      const formattedDate = selectedDate.toISOString().split("T")[0];
-      navigate(`?date=${formattedDate}`, { replace: true });
-    }
-  },[selectedDate, navigate, queryParams])
+  const [member_num1, setMemberNum] = useState(null);
 
-  //카테고리 선택할때마다 바뀌는 운동목록 데이터
   useEffect(() => {
     const category = exerciseCategory === "all" ? `` : `/${exerciseCategory}`;
 
-    axios.get(`/exerciselist`+category)
+    axios.get(`/exerciselist` + category)
       .then((res) => {
-        setExerciseData(res.data.exerList)
+        const validExerciseData = Array.isArray(res.data.exerList) 
+          ? res.data.exerList.filter(item => item.exercise_name)
+          : [];
+        setExerciseData(validExerciseData)
       })
-      .catch((error) => {console.error("운동목록 불러오기 실패", error)});
-  }, [token, exerciseCategory]); // 카테고리가 변경될때마다 데이터 가져오기
+      .catch((error) => { console.error("운동목록 불러오기 실패", error) });
+  }, [token, exerciseCategory]);
 
-  //검색초점
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
 
-  //운동일지 조회 데이터 가져오기
   useEffect(() => {
-      axios.get(`/exercisejournal/${m_calendar_id}`)
-      .then(res=>{
-        setEJournalId(res.data.exercise_id)
-        axios.get(`/membercalendar/${m_calendar_id}`)
-        .then(calendarRes=>{
-          const selectDateCalendar = calendarRes.data.date
-          const filteredData = res.data.filter(item=>selectDateCalendar===selectedDate)
+    axios.get('/membercalendar')
+      .then(res => {
+        const getMaxCalendarId = (data) => {
+          if (!data || data.length === 0) {
+            return null;
+          }
+          return Math.max(...data.map(event => event.m_calendar_id));
+        };
+              
+        const maxCalendarId = getMaxCalendarId(res.data) + 1;
+        setMCalendarIdMax(maxCalendarId);
+        setMCalendarId(res.data.m_calendar_id)
+      })
+      .catch(error => console.log(error));
 
-          const saveLoadData = filteredData.map(data => ({
-            exercise_id: data.exercise_id,
-            exercise_name: data.exercise_name,
-            exercise_set: data.exercise_set,
-            exercise_count: data.exercise_count,
-            exercise_order: data.exercise_order,
-            exercise_weight: data.exercise_weight
-          }))
-          setSelectExercise(saveLoadData)
-        }).catch(error=>console.log(error))
+    axios.get(`/membercalendar`)
+      .then(res => {
+        const formattedSelectedDate = selectedDate.toISOString().split("T")[0];
+        const temp_member_num = res.data[0]?.member_num || null;
+        setMemberNum(temp_member_num)
+        const filteredData = res.data.filter(item => {
+          return item.regdate.split(" ")[0] === formattedSelectedDate && item.memo === "운동";
+        });
+        const mCalendarIds = filteredData.map(item => item.m_calendar_id);
+        let mergedData = [];
+        mCalendarIds.forEach(m_calendar_id => {
+          axios.get(`/exercisejournal/${m_calendar_id}`)
+            .then(res => {
+              mergedData = mergedData.concat(res.data.exerJournalList || []);
+              setSelectExercise([...mergedData]);
+            })
+            .catch(error => {
+              console.error(`exercise Journal API 요청 실패 (m_calendar_id: ${m_calendar_id}):`, error);
+            });
+        })
+      })
+      .catch(error => console.log(error));
+  }, [selectedDate])
 
-    }).catch(error=>console.log(error))
-  },[token,m_calendar_id,selectedDate])//의존성배열
-
-  //운동검색필터
   const exerciseSearch = Array.isArray(exerciseData) 
-  ? exerciseData.filter((data) =>
-      data.exercise_name.toLowerCase().includes(search.toLowerCase())
-    )
-  : [];
+    ? exerciseData.filter((data) =>
+        (data.exercise_name || "").toLowerCase().includes((search || "").toLowerCase())
+      )
+    : [];
 
-  //모달로 운동 상세설명 띄우기
   const handleExerciseDetail = (exercise_id) => {
     axios.get(`/exerciselist/${exercise_id}`)
-    .then(res=>{
-      setModalData(res.data);
-      setShowModal(true);
-    })
-    .catch(error=>console.log(error))
+      .then(res => {
+        setModalData(res.data);
+        setShowModal(true);
+      })  
+      .catch(error => console.log(error))
   }
 
-  //운동목록 클릭시 오른쪽 테이블에 추가되는
   const handleCardClick = (exercise) => {
-    setSelectExercise([...selectExercise, { ...exercise, e_journal_id: `ej_${exercise.exercise_id}_${new Date().getTime()}` }])
-    if (showDetailsOption) {
-      handleExerciseDetail(exercise);
+    if (!selectExercise.find(item => item.exercise_id === exercise.exercise_id)) {
+      setSelectExercise([...selectExercise, { ...exercise, e_journal_id: `ej_${exercise.exercise_id}_${new Date().getTime()}` }]);
     }
   }
 
-  //테이블 드래그하면 순서변경
   const handleOnDragEnd = (result) => {
     if (!result.destination) return;
     const items = Array.from(selectExercise);
@@ -133,13 +122,11 @@ function MemberExerciseAdd() {
     setSelectExercise(items);
   }
 
-  //모달닫기
   const handleCloseModal = () => {
     setShowModal(false);
     setModalData(null);
   };
 
-  //무게 횟수 세트 입력
   const handleInputChange = (e, id, field) => {
     setSelectExercise((prevExercises) =>
       prevExercises.map((exercise) =>
@@ -148,60 +135,80 @@ function MemberExerciseAdd() {
     )
   }
 
-  //날짜변경 핸들러. 선택한 날짜 업데이트되고, url 쿼리 파라미터도 같이 변경됨
-  const handleDateChange = (date)=>{
+  const handleDateChange = (date) => {
     setSelectedDate(date)
     const formattedDate = date.toISOString().split("T")[0]
-    queryParams.set("date",formattedDate)
-    navigate(`?date=${formattedDate}`, { replace: true })
+    navigate(`/member/exerciseadd?date=${formattedDate}`, { replace: true });
+    window.location.reload();
   }
 
-  //저장시 운동등록 되어있으면 수정 아니면 저장
   const handleSubmit = () => {
-    console.log("작동함")
-    selectExercise.forEach((exercise) => {
-      axios.post("/exerciselist", { selectedExercise: [exercise] })
-        .then((res) => {
-          if(res.data.isSuccess){
-            if (exercise.e_journal_id) {
-              console.log("수정할 운동:", exercise.e_journal_id);
-              return axios.post(`/exercisejournal/${exercise.e_journal_id}`, {
-                exercise_id: exercise.exercise_id,
-                exercise_set: exercise.exercise_set,
-                exercise_count: exercise.exercise_count,
-                exercise_order: exercise.exercise_order,
-                exercise_weight: exercise.exercise_weight,
-              })}
-          }
-        })
-        .then(() => {
-          navigate(`/member/exercise/${m_calendar_id}?date=${selectedDate.toISOString().split("T")[0]}`)
-          console.log("운동 추가 및 수정 완료");
-          alert("저장");
-        })
-        .catch((error) => {
-          console.error("에러 발생:", error);
-          alert("에러발생");
-        });
+    const requestData = selectExercise.map(exercise => {
+      return {
+        m_calendar_id: m_calendar_id_max,
+        exercise_name: exercise.name,
+        exercise_id: parseInt(exercise.exercise_id, 10),
+        exercise_set: parseInt(exercise.exercise_set, 10),
+        exercise_count: parseInt(exercise.exercise_count, 10),
+        exercise_order: exercise.exercise_order !== undefined ? parseInt(exercise.exercise_order, 10) : 0,
+        exercise_weight: parseFloat(exercise.exercise_weight, 10),
+      };
     });
+
+    const exercise_add_to_calendar = {
+      member_num: member_num1,
+      m_calendar_id: m_calendar_id_max,
+      memo: "운동",
+      regdate: selectedDate.toISOString().split("T")[0],
+    };
+
+    axios.post('/membercalendar', exercise_add_to_calendar)
+      .then((res) => {
+        console.log("calend", res)
+      })
+      .catch(error => {
+        console.log(error);
+        alert("저장 에러");
+      });
+                
+    axios.post("/exercisejournal", requestData, {})
+      .then((res) => {
+        if (res.status === 200 && res.data.isSuccess) {
+          console.log("운동 추가 및 수정 완료");
+        } else {
+          console.error("응답 실패:", res.data);
+          alert("저장에 실패했습니다.");
+        }
+      })
+      .catch((error) => {
+        console.error("에러 발생:", error);
+        if (error.response) {
+          console.error("응답 데이터:", error.response.data);
+          console.error("응답 상태 코드:", error.response.status);
+          console.error("응답 헤더:", error.response.headers);
+          alert(`에러 발생: ${JSON.stringify(error.response.data)}`);
+        } else {
+          console.error("에러 메시지:", error.message);
+          alert(`에러 발생: ${error.message}`);
+        }
+      });
   };  
-  
 
   return (
     <>
-    <Row>
+      <Row>
         <Col>
           <Card>
-           <Card.Header as="h6" className="border-bottom p-3 mb-0">
-                <h2>{selectedDate.toLocaleDateString('ko-KR', { year: 'numeric', month: 'long', day: 'numeric' })}의 운동 추가</h2>
-                <div style={{ marginBottom: "20px" }}>
-                    <DatePicker
-                    selected={selectedDate}  // 현재 선택된 날짜 설정
-                    onChange={handleDateChange}  // 날짜 변경 시 handleDateChange 호출
-                    dateFormat="yyyy년 MM월 dd일"  // 날짜 포맷 설정
-                    placeholderText="날짜를 선택하세요"
-                    />
-                </div>
+            <Card.Header as="h6" className="border-bottom p-3 mb-0">
+              <h2>{selectedDate.toLocaleDateString('ko-KR', { year: 'numeric', month: 'long', day: 'numeric' })}의 운동 추가</h2>
+              <div style={{ marginBottom: "20px" }}>
+                <DatePicker
+                  selected={selectedDate}
+                  onChange={handleDateChange}
+                  dateFormat="yyyy년 MM월 dd일"
+                  placeholderText="날짜를 선택하세요"
+                />
+              </div>
             </Card.Header>
           </Card>
         </Col>
@@ -226,24 +233,24 @@ function MemberExerciseAdd() {
                 </DropdownButton>
                 <Form.Control onChange={handleChange} placeholder="운동목록 검색" type="text"/>
               </InputGroup>
-
+        
               <Form.Group controlId="showDetailsCheckbox" className="mb-3">
                 <Form.Check type="checkbox" label="운동 상세 보기" checked={showDetailsOption} onChange={(e) => setShowDetailsOption(e.target.checked)}/>
               </Form.Group>
 
-                <Row>
-                  {exerciseData.map((data) => (
-                    <Col key={data.exercise_id} xs={12} sm={6} md={4} lg={4} className="mb-3">
-                      <Card onClick={() => handleCardClick(data)} style={{ cursor: "pointer" }}
-                        className={`h-100 ${selectExercise.find((item) => item.exercise_id === data.exercise_id) ? "border-primary" : ""}`}>
-                        <Card.Img variant="top" src={`http://52.78.38.12:8080/upload/${data.exercise_image}`} alt={data.exercise_name} style={{ height: "150px", objectFit: "cover" }}/>
-                        <Card.Body className="d-flex justify-content-center align-items-center p-2">
-                          <Card.Text className="text-center">{data.exercise_name}</Card.Text>
-                        </Card.Body>
-                      </Card>
-                    </Col>
-                  ))}
-                </Row>
+              <Row>
+                {exerciseSearch.map((data) => (
+                  <Col key={data.exercise_id} xs={12} sm={6} md={4} lg={4} className="mb-3">
+                    <Card onClick={() => handleCardClick(data)} style={{ cursor: "pointer" }}
+                      className={`h-100 ${selectExercise.find((item) => item.exercise_id === data.exercise_id) ? "border-primary" : ""}`}>
+                      <Card.Img variant="top" src={`http://52.78.38.12:8080/upload/${data.exercise_image}`} alt={data.exercise_name} style={{ height: "150px", objectFit: "cover" }}/>
+                      <Card.Body className="d-flex justify-content-center align-items-center p-2">
+                        <Card.Text className="text-center">{data.exercise_name}</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
             </Card.Body>
           </Card>
         </Col>
@@ -283,15 +290,15 @@ function MemberExerciseAdd() {
                               >
                                 <td>{data.exercise_name}</td>
                                 <td>
-                                  <Form.Control type="number" value={data.exercise_weight || ""} placeholder="무게"
+                                  <Form.Control name="exercise_weight" type="number" value={data.exercise_weight || ""} placeholder="무게"
                                     onChange={(e) => handleInputChange(e, data.e_journal_id, "exercise_weight")}/>
                                 </td>
                                 <td>
-                                  <Form.Control type="number" value={data.exercise_count || ""} placeholder="횟수"
+                                  <Form.Control name="exercise_count" type="number" value={data.exercise_count || ""} placeholder="횟수"
                                     onChange={(e) => handleInputChange(e, data.e_journal_id, "exercise_count")}/>
                                 </td>
                                 <td>
-                                  <Form.Control type="number" value={data.exercise_set || ""} placeholder="세트"
+                                  <Form.Control name="exercise_set" type="number" value={data.exercise_set || ""} placeholder="세트"
                                     onChange={(e) => handleInputChange(e, data.e_journal_id, "exercise_set")}/>
                                 </td>
                                 <td>{index + 1}</td>
--- FrontEnd/fitconnect/src/routes/Router.js
+++ FrontEnd/fitconnect/src/routes/Router.js
@@ -9,7 +9,7 @@ import MemberSignUp from '../pages/User/MemberSignUp';
 import TrainerId from '../pages/User/TrainerId';
 import GoogleLogin from '../pages/User/GoogleLogin';
 import MemberMain from '../pages/Member/MemberMain';
-import MemberMypage from '../pages/Member/MemberMypage';
+import MemberMyPage from '../pages/Member/MemberMyPage';
 import MemberMypageDetail from '../pages/Member/MemberMypageDetail';
 import MemberTrainerList from '../pages/Member/MemberTrainerList';
 import MemberCalendar from '../pages/Member/MemberCalendar';
@@ -24,10 +24,11 @@ import TrainerMypage from '../pages/Trainer/TrainerMypage';
 import TrainerMypageDetail from '../pages/Trainer/TrainerMypageDetail';
 import TrainerMain from '../pages/Trainer/TrainerMain';
 import AdminMain from '../pages/Admin/AdminMain';
-import ProtectedRoute from "../components/ProtectedRoute";
 
- //ProtectedRoute 사용할 경우 이 주석을 이용해 감싸서 사용해주기  MEMBER,TRAINER,ADMIN 대문자로!
- // 예시: <ProtectedRoute allowedRoles={['ADMIN']}><TrainerId/></ProtectedRoute>
+import ProtectedRoute from '../components/ProtectedRoute'; // ProtectedRoute 컴포넌트 임포트
+
+// ProtectedRoute 사용할 경우 이 주석을 이용해 감싸서 사용해주기  MEMBER, TRAINER, ADMIN 대문자로!
+// <ProtectedRoute allowedRoles={['MEMBER']}><TrainerId/></ProtectedRoute>
 
 /********* Routes *******/
 const Routes = [
@@ -38,36 +39,148 @@ const Routes = [
       { path: "*", element: <Navigate to="/" /> },
       { path: "/", element: <UserMain /> },
       { path: "/login", element: <UserLogin /> },
-      { path: "/signup", element: <UserSignUp/> },
+      { path: "/signup", element: <UserSignUp /> },
       { path: "/trainersignup", element: <TrainerSignUp /> },
       { path: "/membersignup", element: <MemberSignUp /> },
-      { path: "/trainerid", element: <ProtectedRoute allowedRoles={['MEMBER']}><TrainerId /></ProtectedRoute> },
-      { path: "/googlelogin", element: <GoogleLogin/>},
+      { path: "/trainerid", element: <TrainerId /> },
+      { path: "/googlelogin", element: <GoogleLogin /> },
 
       // 멤버 관련 경로
       { path: "/member/*", element: <Navigate to="/member" /> },
-      { path: "/member", element: <ProtectedRoute allowedRoles={['MEMBER']}><MemberMain /></ProtectedRoute> },
-      { path: "/member/mypage", element: <ProtectedRoute allowedRoles={['MEMBER']}><MemberMypage /></ProtectedRoute> },
-      { path: "/member/mypagedetail", element: <ProtectedRoute allowedRoles={['MEMBER']}><MemberMypageDetail /></ProtectedRoute> },
-      { path: "/member/trainerlist/:member_num", element: <ProtectedRoute allowedRoles={['MEMBER']}><MemberTrainerList /></ProtectedRoute> },
-      { path: "/member/calendar", element: <ProtectedRoute allowedRoles={['MEMBER']}><MemberCalendar /></ProtectedRoute> },
-      { path: "/member/dietjournal", element: <ProtectedRoute allowedRoles={['MEMBER']}><MemberDietJournal /></ProtectedRoute> },
-      { path: "/member/dietadd", element: <ProtectedRoute allowedRoles={['MEMBER']}><MemberDietAdd /></ProtectedRoute> },
-      { path: "/member/exercise", element: <ProtectedRoute allowedRoles={['MEMBER']}><MemberExercise /></ProtectedRoute> },
-      { path: "/member/exerciseadd", element: <ProtectedRoute allowedRoles={['MEMBER']}><MemberExerciseAdd /></ProtectedRoute> },
+      {
+        path: "/member",
+        element: (
+          <ProtectedRoute allowedRoles={['MEMBER']}>
+            <MemberMain />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/member/mypage",
+        element: (
+          <ProtectedRoute allowedRoles={['MEMBER']}>
+            <MemberMyPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/member/mypagedetail",
+        element: (
+          <ProtectedRoute allowedRoles={['MEMBER']}>
+            <MemberMypageDetail />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/member/trainerlist",
+        element: (
+          <ProtectedRoute allowedRoles={['MEMBER']}>
+            <MemberTrainerList />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/member/calendar",
+        element: (
+          <ProtectedRoute allowedRoles={['MEMBER']}>
+            <MemberCalendar />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/member/dietjournal",
+        element: (
+          <ProtectedRoute allowedRoles={['MEMBER']}>
+            <MemberDietJournal />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/member/dietadd",
+        element: (
+          <ProtectedRoute allowedRoles={['MEMBER']}>
+            <MemberDietAdd />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/member/exercise",
+        element: (
+          <ProtectedRoute allowedRoles={['MEMBER']}>
+            <MemberExercise />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/member/exerciseadd",
+        element: (
+          <ProtectedRoute allowedRoles={['MEMBER']}>
+            <MemberExerciseAdd />
+          </ProtectedRoute>
+        ),
+      },
 
       // 트레이너 관련 경로
       { path: "/trainer/*", element: <Navigate to="/trainer" /> },
-      { path: "/trainer", element: <ProtectedRoute allowedRoles={['TRAINER']}><TrainerMain/></ProtectedRoute> },
-      { path: "/trainer/calendar", element: <ProtectedRoute allowedRoles={['TRAINER']}><TrainerCalendar /></ProtectedRoute> },
-      { path: "/trainer/message", element: <ProtectedRoute allowedRoles={['TRAINER']}><TrainerMessage/></ProtectedRoute>},
-      { path: "/trainer/members", element: <ProtectedRoute allowedRoles={['TRAINER']}><TrainerMembers/></ProtectedRoute>},
-      { path: "/trainer/mypage", element: <ProtectedRoute allowedRoles={['TRAINER']}><TrainerMypage/></ProtectedRoute>},
-      { path: "/trainer/mypagedetail", element: <ProtectedRoute allowedRoles={['TRAINER']}><TrainerMypageDetail/></ProtectedRoute>},
+      {
+        path: "/trainer",
+        element: (
+          <ProtectedRoute allowedRoles={['TRAINER']}>
+            <TrainerMain />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/trainer/calendar",
+        element: (
+          <ProtectedRoute allowedRoles={['TRAINER']}>
+            <TrainerCalendar />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/trainer/message",
+        element: (
+          <ProtectedRoute allowedRoles={['TRAINER']}>
+            <TrainerMessage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/trainer/members",
+        element: (
+          <ProtectedRoute allowedRoles={['TRAINER']}>
+            <TrainerMembers />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/trainer/mypage",
+        element: (
+          <ProtectedRoute allowedRoles={['TRAINER']}>
+            <TrainerMypage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/trainer/mypagedetail",
+        element: (
+          <ProtectedRoute allowedRoles={['TRAINER']}>
+            <TrainerMypageDetail />
+          </ProtectedRoute>
+        ),
+      },
 
       // 관리자 관련 경로
       { path: "/admin/*", element: <Navigate to="/admin" /> },
-      { path: "/admin", element: <ProtectedRoute allowedRoles={['ADMIN']}><AdminMain/></ProtectedRoute> }
+      {
+        path: "/admin",
+        element: (
+          <ProtectedRoute allowedRoles={['ADMIN']}>
+            <AdminMain />
+          </ProtectedRoute>
+        ),
+      },
     ],
   },
 ];
