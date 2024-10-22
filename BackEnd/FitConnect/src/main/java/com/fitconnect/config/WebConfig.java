544d38495ebba579ab4e60bfe002fec3bb53d347
yujun <did124@naver.com>
2024-10-22 10:45:04 +0900

    롤백

--- BackEnd/FitConnect/src/main/java/com/fitconnect/config/WebConfig.java
+++ BackEnd/FitConnect/src/main/java/com/fitconnect/config/WebConfig.java
@@ -10,19 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        // API가 아닌 SPA 경로 요청만 index.html로 포워드
-		 registry.addViewController("/{path:[a-zA-Z0-9-_]+}")
-         .setViewName("forward:/index.html");
- 
-		 // 여러 경로로 구성된 요청 처리
-		 registry.addViewController("/{path:[a-zA-Z0-9-_]+}/**")
-		         .setViewName("forward:/index.html");
-		}
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // 정적 리소스 경로를 명시적으로 매핑
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/static/");
+		// 모든 경로를 index.html로 매핑
+        registry.addViewController("/{spring:[^\\.]*}").setViewName("forward:/index.html");
     }
+
 }
